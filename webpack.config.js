9beedd83ebac90a0940abeb19fb1a1be1c584615
Pawel Krzywda <paw.krzywda@gmail.com>
2020-12-02 16:55:17 +0100

    webpack version changed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { merge } = require('webpack-merge');
 const commonConfiguration = require('./webpack/common');
 
 module.exports = (_env, { mode }) => {
-  const properlyConfig = require('./webpack/${mode}');
+  const properlyConfig = require(`./webpack/${mode}`);
   const mergedConfig = merge(commonConfiguration, properlyConfig);
 
   return mergedConfig;
