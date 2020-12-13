8cac6e3f6334a7607a1254411892ac2cd34ac8b9
Pawel Krzywda <paw.krzywda@gmail.com>
2020-12-13 18:26:24 +0100

    feat: User courses

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -5,6 +5,7 @@ import { default as ContentStyles } from './Content.module.scss';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { StoreContext } from '../../store/StoreProvider';
 import Courses from '../Courses/Courses';
+import UserCourses from '../UserCourses/UserCourses';
 
 const style = bemCssModules(ContentStyles);
 
@@ -20,7 +21,7 @@ const Content = () => {
     <main className={style()}>
       <Switch>
         <Route exact path="/" render={() => <Courses />} />
-        {isUserLogged && <Route exact path="/my-courses" render={() => <p>Moje kursy</p>} />}
+        {isUserLogged && <Route exact path="/my-courses" render={() => <UserCourses />} />}
         {isAdmin && <Route exact path="/manage-courses" render={() => <p>Zarządzanie kursami</p>} />}
         <Redirect to="/" />
       </Switch>
---
+++ src/components/UserCourses/UserCourses.jsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import bemCssModules from 'bem-css-modules';
+
+import { default as UserCoursesStyles } from './UserCourses.module.scss';
+import { StoreContext } from '../../store/StoreProvider';
+import Course from '../Course/Course';
+
+const style = bemCssModules(UserCoursesStyles);
+
+const UserCourses = () => {
+  const { user, courses } = useContext(StoreContext);
+
+  const buyedCourses = courses
+    .filter(course => user.courses.includes(course.id))
+    .map(course => <Course key={course.id} {...course} />)
+
+  return (
+    <section className={style()}>
+      <h2 className={style('title')}>Twoje wykupione kursy</h2>
+      <ul className={style('list')}>
+        {buyedCourses}
+      </ul>
+    </section>
+  );
+};
+
+export default UserCourses;
